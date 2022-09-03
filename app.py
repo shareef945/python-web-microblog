45c2003dacd87d94a664cbc0998bb48f81a59791
shareef945 <64172657+shareef945@users.noreply.github.com>
2022-09-03 15:10:50 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import datetime
+from pydoc import render_doc
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient("mongodb+srv://shareef:BtO0b6BmeAjHBp15@microblog-application.nj9llpg.mongodb.net/test")
+    app.db = client.microblog
+
+    @app.route('/', methods=["GET","POST"])
+    def home():
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+
+        entries_with_date = [(
+            entry["content"],
+            entry['date'],
+            datetime.datetime.strptime(entry['date'], "%Y-%m-%d").strftime("%b %d")
+        )
+            for entry in app.db.entries.find({})
+        ]
+
+        return render_template('home.html', entries = entries_with_date)
+
+    return app
